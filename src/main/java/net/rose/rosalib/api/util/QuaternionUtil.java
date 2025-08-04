5d282eb6de9fedbd2e763b9d0a262c95dd4b14ae
Rose <skillwarzaking@gmail.com>
2025-08-04 11:33:31 +0200

    🫧 Refactored quaternion related method names for consistency: `QuaternionUtil#eulerAngles` -> `QuaternionUtil#eulerAngleDeg` `QuaternionUtil#eulerAngleRadians` -> `QuaternionUtil#eulerAngleRad`

--- src/main/java/net/rose/rosalib/api/util/QuaternionUtil.java
+++ src/main/java/net/rose/rosalib/api/util/QuaternionUtil.java
@@ -14,10 +14,10 @@ public class QuaternionUtil {
      * @param y The Y rotation of the quaternion.
      * @param z The Z rotation of the quaternion.
      * @return A new quaternion using those x y and z rotations.
-     * @see #eulerAngleRadians(float, float, float)
+     * @see #eulerAngleRad(float, float, float)
      */
-    public static Quaternionf eulerAngles(double x, double y, double z) {
-        return eulerAngleRadians((float) Math.toRadians(x), (float) Math.toRadians(y), (float) Math.toRadians(z));
+    public static Quaternionf eulerAngleDeg(double x, double y, double z) {
+        return eulerAngleRad((float) Math.toRadians(x), (float) Math.toRadians(y), (float) Math.toRadians(z));
     }
 
     /**
@@ -28,9 +28,9 @@ public class QuaternionUtil {
      * @param y The Y rotation of the quaternion.
      * @param z The Z rotation of the quaternion.
      * @return A new quaternion using those x y and z rotations.
-     * @see #eulerAngles(double, double, double)
+     * @see #eulerAngleDeg(double, double, double)
      */
-    public static Quaternionf eulerAngleRadians(float x, float y, float z) {
+    public static Quaternionf eulerAngleRad(float x, float y, float z) {
         return new Quaternionf().rotationXYZ(x, y, z);
     }
 }
